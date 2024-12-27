c413d7fad126f1c3e991761bf3453e2c751d9810
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-12-27 23:07:59 +0500

    Add union by size in DSU class

--- DSU/disjoint_set_union.py
+++ DSU/disjoint_set_union.py
@@ -2,7 +2,8 @@ class DSU:
 
     def __init__(self, n):
         self.parent = list(range(n))
-        self.rank = [0]*n
+        self.rank = [0] * n     # for union by rank
+        # self.rank = [1] * n   # for union by size
 
     def union(self, a, b):
         par_a = self.find(a)
@@ -17,6 +18,15 @@ class DSU:
             else:
                 self.parent[par_b] = self.parent[par_a]
                 self.rank[par_a] += 1
+
+        # # Union by size
+        # if par_a != par_b:
+        #     if self.rank[par_a] < self.rank[par_b]:
+        #         self.parent[par_a] = self.parent[par_b]
+        #         self.rank[par_b] += self.rank[par_a]
+        #     else:
+        #         self.parent[par_b] = self.parent[par_a]
+        #         self.rank[par_a] += self.rank[par_b]
 
     def find(self, a):
         if self.parent[a] != a:
