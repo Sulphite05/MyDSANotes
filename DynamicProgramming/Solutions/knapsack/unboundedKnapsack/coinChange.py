5bb4e2c419057feac0c3c9709cf6e19610ff9844
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2025-03-12 15:20:14 +0500

    Add comments to coin change 1

--- DynamicProgramming/Solutions/knapsack/unboundedKnapsack/coinChange.py
+++ DynamicProgramming/Solutions/knapsack/unboundedKnapsack/coinChange.py
@@ -6,8 +6,10 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
         dp = [[0] * (amount + 1) for _ in range(n + 1)]
-        dp[0] = [float('inf')] * (amount + 1)
-        dp[0][0] = 0
+        dp[0] = [float('inf')-1] * (amount + 1)
+        # We cannot make the required sum with 0 coins so infinite coins is the case here
+        # We do -1 to prevent overflow when we are adding 1 below
+        dp[0][0] = 0    # However, We can create an amount of 0 by not using any coin at all
         for i in range(1, n + 1):  # coin
             for j in range(1, amount + 1):  # curr sum
                 if coins[i - 1] <= j:
