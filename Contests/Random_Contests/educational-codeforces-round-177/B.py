414f6f2c070f914b27293ae29f34d8e3cfd4af24
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2025-04-05 19:15:24 +0500

    Add B

--- Contests/Random_Contests/educational-codeforces-round-177/B.py
+++ Contests/Random_Contests/educational-codeforces-round-177/B.py
@@ -2,33 +2,15 @@ t = int(input())
 for _ in range(t):
     n, k, x = list(map(int, input().strip().split()))
     lst = list(map(int, input().strip().split()))[::-1]
+    summ = sum(lst)
     maxi = n*k
-    minus = 0
-    summ = 0
-    ans = 0
-    ex = 0
+    full = x // summ
+    tot = full * summ
+    add = 0
     for num in lst:
-        summ += num
-        if summ >= x:
-            ex = True
-            break
-        minus += 1
-
-    if not ex:
-        req = x / summ
-        if req > k:
-            print(0)
-        else:
-            req = int(req)
-            minus = req * n
-            curr = summ * req
-            for num in lst:
-                curr += num
-                if curr >= x:
-                    break
-                minus += 1
-            ans = maxi - minus
-            print(ans)
-    else:
-        print(maxi - minus)
-
+        if tot < x:
+            tot += num
+            add += 1
+        else: break
+    res = maxi - full * n - add + 1
+    print(max(0, res))
